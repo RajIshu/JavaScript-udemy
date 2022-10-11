5287e05c11f46dcffaa8c1a916063981f4c5f815
RajIshu <gokuishu57@gmail.com>
2022-10-12 00:41:16 +0530

    add MyNotes

---
+++ MyNotes/001 - JavaScriptFundamentalsPart1/script.js
@@ -0,0 +1,66 @@
+let js = 'amazing'; // Variable Declaration
+if(js === 'amazing') alert("JavaScript is FUN!");
+
+40 + 8 + 23 - 10 // It will be not visible anywhere
+
+// we have to define where we want to see the result
+console.log(40 + 8 + 23 - 10); // It will print the result in brower comsole window 
+console.log('Ishu');
+console.log(23);
+
+/* 
+VALUES (in these lines of code):
+------------------------------- 
+    console.log(23); --> 23
+    console.log('Ishu'); --> 'Ishu'
+    console.log(40 + 8 + 23 - 10); --> 40, 8, 23, 10
+        --> These Mathematical operator combine these values to form ONE value i.e., 61.
+
+VARIABLES:
+---------
+    - Values can be stored in Variables so that we can reuse them over and over again.
+
+*/
+
+// Declaring a VARIABLE:
+let firstName = 'Ishu';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+/*
+Naming Convention for Variables:
+------------------------------
+1. camelCase : 
+    - firstName, theCompleteJavaScriptCourse
+
+   snake_case:
+    - first_name, the_complete_javascript_course
+
+2. No start with digit or numeric values or number
+   Eg: 3years, 2boys, etc.
+   
+3. Variable names can include numbers, letters, underscores, dollar sign.
+
+4. Don't use Reserved JavaScript keywords.
+   Eg: let, function, new, etc. 
+
+5. Never start variable name with uppercase letters. (They are used in OOPs)
+   Eg: Person, FirstName, etc.
+
+6. Variables in all UpperCase are Constants.
+   Eg: PI (= 3.141592653589793),
+       SQRT2 (= 1.4142135623730951),
+       E (= 2.718281828459045), etc.
+
+7. Variable name should be meaningful.
+   Eg:
+    Good Way:
+        let myFirstJob = 'Programmer';
+        let myFirstJob = 'Teacher';
+    Bad Way:
+        let job1 = 'Programmer';
+        let job2 = 'Teacher';
+
+*/
